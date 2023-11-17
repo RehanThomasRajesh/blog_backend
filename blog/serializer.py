bdaec033889bbba6b6cf6001129f26e27988c8b1
RehanThomasRajesh <rehanthomas1@gmail.com>
2023-11-17 23:42:54 +0530

    'finalcommit'

--- blog/serializer.py
+++ blog/serializer.py
@@ -4,10 +4,10 @@ from blog.models import user,post
 class userSerializer(serializers.ModelSerializer):
     class Meta:
         model = user
-        fields = ('id', 'name','profile','email','password')
+        fields = ('name','profile','email','password')
 
 
 class postSerializer(serializers.ModelSerializer):
     class Meta:
         model = post
-        fields = ('title', 'id','message')
+        fields = ('title', 'user_id','message')
